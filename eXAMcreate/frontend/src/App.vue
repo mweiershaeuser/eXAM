624c8b2a718ae7371da8e3dc38c2f5dd582dc8fc
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-04-29 16:54:12 +0200

    added project structure for eXAMcreate frontend

---
+++ eXAMcreate/frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
