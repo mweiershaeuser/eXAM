1098188104ccdbdd3359e39cf4d119725af689a9
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-05-29 18:48:47 +0200

    added eXAMmanage backend endpoint to statService as optional environment variable via Docker

--- statService/main.ts
+++ statService/main.ts
@@ -2,8 +2,11 @@ import { Application, Router } from "./deps.ts";
 
 const router = new Router({ prefix: "/api/statService" });
 
+const exammanageApiBaseUrl =
+  Deno.env.get("EXAMMANAGE_API_BASE_URL") ?? "http://localhost:8080";
+
 router.get("/stats", async (context) => {
-  const exams = await fetch("http://localhost:8080/exams", {
+  const exams = await fetch(`${exammanageApiBaseUrl}/exams`, {
     headers: {
       "Content-Type": "application/json",
     },
